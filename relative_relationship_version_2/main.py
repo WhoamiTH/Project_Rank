67ad25fc2ea8a1733e110b465bc9651fd9d91d24
taihann1 <taihann1@gmail.com>
2018-07-30 12:10:38 +1000

    mirror

--- relative_relationship_version_2/main.py
+++ relative_relationship_version_2/main.py
@@ -15,7 +15,8 @@ def train_model(train_data, train_label, test_data, test_label):
         model = sklin.LogisticRegression()
         model.fit(train_data,train_label.flatten())
     if model_type == 'SVC':
-        model = sksvm.SVC(C=0.1,kernel='rbf')
+        model = sksvm.SVC(C=0.1,kernel='linear')
+        # model = sksvm.SVC(C=0.1,kernel='rbf')
         # model = sksvm.SVC(C=0.1,kernel='poly')
         model.fit(train_data, train_label.flatten())
     if model_type == 'DT':
@@ -32,9 +33,14 @@ def crossvalidation(percent, Ds, Dl, Data, Label, record):
     global negative_value
     global threshold_value
     global mirror_type
+    #-------------------------------------------------------
+    global result_number
+    global winner_number
+    #-------------------------------------------------------
     num_of_train = math.ceil(len(Ds) * percent)
     for i in range(rep_times):
         all_group_top_precision = []
+        all_group_recall = []
         all_group_top_exact_accuracy = []
         all_group_exact_accuracy = []
         start = clock()
@@ -49,11 +55,11 @@ def crossvalidation(percent, Ds, Dl, Data, Label, record):
         record.write(str(model) + "\n")
         record.write("-------------------------------------------------------------------------------------------\n")
         # predict_test.general_test(test_data, test_label, model, positive_value, negative_value, threshold_value, record)
-        all_group_top_precision, all_group_top_exact_accuracy, all_group_exact_accuracy = \
-            predict_test.group_test(new_data, Label, Ds, Dl, num_of_train, model, threshold_value,
-                                    top, all_group_top_precision, all_group_top_exact_accuracy, all_group_exact_accuracy, record)
+        all_group_top_precision, all_group_recall, all_group_top_exact_accuracy, all_group_exact_accuracy = \
+            predict_test.group_test(new_data, Label, Ds, Dl, num_of_train, model, threshold_value, result_number, winner_number,
+                                    all_group_top_precision, all_group_recall, all_group_top_exact_accuracy, all_group_exact_accuracy, record)
         running_time = finish-start
-        predict_test.cal_average(all_group_top_precision, all_group_top_exact_accuracy, all_group_exact_accuracy, record)
+        predict_test.cal_average(all_group_top_precision, all_group_recall, all_group_top_exact_accuracy, all_group_exact_accuracy, record)
         record.write("the {0} time training time is {1}\n".format(i+1,training_time))
         record.write("the {0} time running time is {1}\n".format(i+1,running_time))
         record.write("-------------------------------------------------------------------------------------------\n\n\n")
@@ -124,8 +130,8 @@ def set_para():
 model_type = 'LR'
 # model_type = 'SVC'
 # model_type = 'DT'
-# mirror_type = "mirror"
-mirror_type = "not_mirror"
+mirror_type = "mirror"
+# mirror_type = "not_mirror"
 top = 3
 rep_times = 3
 positive_value = 1
@@ -135,6 +141,11 @@ kernelpca_or_not = False
 pca_or_not = False
 num_of_components = 20
 percentage = 0
+
+#------------------------
+result_number = 8
+winner_number = 3
+#------------------------
 
 # ----------------------------------set parameters--------------------------------------------------------------------
 set_para()
@@ -149,7 +160,7 @@ record_name = path
 
 
 if percentage != 0:
-    record_name += '_result_percent_' + str(percentage)
+    record_name += mirror_type + '_result_percent_' + str(percentage)
     if pca_or_not:
         record_name += '_pca_' + str(num_of_components)
     if kernelpca_or_not:
@@ -161,7 +172,7 @@ else:
         percentage = i/10
         # percent = 0.9
         # record_name = path + str(percent) + '_' + str(i) + '.txt'
-        record_name += '_result_percent_' + str(percentage)
+        record_name += '_' + mirror_type + '_result_percent_' + str(percentage)
         if pca_or_not:
             record_name += '_pca_' + str(num_of_components)
         if kernelpca_or_not:
--- relative_relationship_version_2/predict_test.py
+++ relative_relationship_version_2/predict_test.py
@@ -27,12 +27,35 @@ def count_general_pre(y_true, y_pred, positive_value, negative_value, threshold_
     return tp, fp, fn
 
 
-def count_top(y_true, y_pred, top):
+# def count_top(y_true, y_pred, top):
+#     tp = 0
+#     exact = 0
+#     if top <= len(y_true):
+#         top_true = y_true[:top]
+#         top_pred = y_pred[:top]
+#     else:
+#         top_true = y_true
+#         top_pred = y_pred
+#     len_top = len(top_pred)
+#     for i in range(len_top):
+#         if top_pred[i] in top_true:
+#             tp += 1
+#             if top_pred[i] == top_true[i]:
+#                 exact += 1
+#     group_top_pre = tp/top
+#     group_top_exact_accuracy = exact/top
+#     return group_top_pre, group_recall, group_top_exact_accuracy
+
+
+def count_top(y_true, y_pred, result_number, winner_number):
     tp = 0
     exact = 0
-    if top <= len(y_true):
-        top_true = y_true[:top]
-        top_pred = y_pred[:top]
+    if result_number <= len(y_true):
+        top_true = y_true[:winner_number]
+        top_pred = y_pred[:result_number]
+    elif len(y_true) >= winner_number:
+        top_true = y_true[:winner_number]
+        top_pred = y_pred
     else:
         top_true = y_true
         top_pred = y_pred
@@ -40,11 +63,24 @@ def count_top(y_true, y_pred, top):
     for i in range(len_top):
         if top_pred[i] in top_true:
             tp += 1
-            if top_pred[i] == top_true[i]:
-                exact += 1
-    group_top_pre = tp/top
-    group_top_exact_accuracy = exact/top
-    return group_top_pre, group_top_exact_accuracy
+            if i < winner_number:
+                if top_pred[i] == top_true[i]:
+                    exact += 1
+    if result_number == len_top:
+        group_pre = tp/result_number
+        group_recall = tp/winner_number
+        group_top_exact_accuracy = exact/winner_number
+    elif len_top >= winner_number:
+        group_pre = tp/len_top
+        group_recall = tp/winner_number
+        group_top_exact_accuracy = exact/winner_number
+    else:
+        group_pre = tp/len_top
+        group_recall = tp/len_top
+        group_top_exact_accuracy = exact/len_top
+    return group_pre, group_recall, group_top_exact_accuracy
+
+
 
 
 def precision_recall(tp, fp, fn):
@@ -78,28 +114,49 @@ def record_middle_result(name, list, record):
     record.write('\n')
 
 
-def rank_the_group(group_data, reference, model, threshold, record):
-    low = []
-    high = []
-    if len(reference) <= 1:
+# def rank_the_group(group_data, reference, model, threshold, record):
+#     low = []
+#     high = []
+#     if len(reference) <= 1:
+#         return reference
+#     else:
+#         pivot = random.choice(reference)
+#         # record.write(f"the pivot is          {pivot:2d}\n")
+#         reference.remove(pivot)
+#         for i in reference:
+#             t = handle_data.data_extend(group_data[pivot-1], group_data[i-1])
+#             t = np.array(t).reshape((1,-1))
+#             if model.predict(t) > threshold:
+#                 low.append(i)
+#             else:
+#                 high.append(i)
+#     # record_middle_result('low', low, record)
+#     # record_middle_result('high', high, record)
+#     low = rank_the_group(group_data, low, model, threshold, record)
+#     high = rank_the_group(group_data, high, model, threshold, record)
+#     high.append(pivot)
+#     return high + low
+
+
+
+def rank_the_group(group_data, reference, model, threshold, result_number, record):
+    if len(reference) <= result_number:
         return reference
     else:
-        pivot = random.choice(reference)
-        # record.write(f"the pivot is          {pivot:2d}\n")
-        reference.remove(pivot)
-        for i in reference:
-            t = handle_data.data_extend(group_data[pivot-1], group_data[i-1])
-            t = np.array(t).reshape((1,-1))
-            if model.predict(t) > threshold:
-                low.append(i)
-            else:
-                high.append(i)
-    # record_middle_result('low', low, record)
-    # record_middle_result('high', high, record)
-    low = rank_the_group(group_data, low, model, threshold, record)
-    high = rank_the_group(group_data, high, model, threshold, record)
-    high.append(pivot)
-    return high + low
+        tem = [reference.pop()]
+        for each in reference:
+            for item in range(len(tem)):
+                t = handle_data.data_extend(group_data[each-1], group_data[tem[item]-1])
+                t = np.array(t).reshape((1,-1))
+                if model.predict(t) > threshold:
+                    tem.insert(item, each)
+                    break
+                else:
+                    if item == len(tem)-1:
+                        tem.append(each)
+                        break
+        return tem
+
 
 
 def record_rank_reference(reference, rank, predict_rank, record):
@@ -110,7 +167,7 @@ def record_rank_reference(reference, rank, predict_rank, record):
     record_middle_result('the predict rank is   ', predict_rank, record)
 
 
-def group_test(Data, Label, Ds, Dl, num_of_train, model, threshold_value, top, all_group_top_precision, all_group_top_exact_accuracy, all_group_exact_accuracy, record):
+def group_test(Data, Label, Ds, Dl, num_of_train, model, threshold_value, result_number, winner_number, all_group_top_precision, all_group_recall, all_group_top_exact_accuracy, all_group_exact_accuracy, record):
     for group_index_start in range(num_of_train,len(Ds)):
         group_start = Ds[group_index_start]
         length = Dl[group_index_start]
@@ -119,33 +176,40 @@ def group_test(Data, Label, Ds, Dl, num_of_train, model, threshold_value, top, a
         group_label = Label[group_start:group_end]
         reference = [t for t in range(1, length + 1)]
         random.shuffle(reference)
-        predict_rank = rank_the_group(group_data, reference, model, threshold_value, record)
+        predict_rank = rank_the_group(group_data, reference, model, threshold_value, result_number, record)
         group_rank = handle_data.exchange(group_label)
         # reference = [t for t in range(1, length + 1)]
         # record_rank_reference(reference, group_rank, predict_rank, record)
         # --------------------------------------------------------------------
         # group_test_relative(group_data, group_label, length, model, threshold_value, record)
         # --------------------------------------------------------------------
-        group_top_precision, group_top_exact_accuracy = count_top(group_rank, predict_rank, top)
+        group_top_precision, group_recall, group_top_exact_accuracy = count_top(group_rank, predict_rank, result_number, winner_number)
         group_exact_accuracy = calacc(group_rank, predict_rank)
         all_group_top_precision.append(group_top_precision)
+        all_group_recall.append(group_recall)
         all_group_top_exact_accuracy.append(group_top_exact_accuracy)
         all_group_exact_accuracy.append(group_exact_accuracy)
+
         # record.write("the group top precision is {0}\n".format(group_top_precision))
+        # record.write("the group recall is {0}\n".format(group_recall))
         # record.write("the group top exact accuracy is {0}\n".format(group_top_exact_accuracy))
         # record.write("the group accuracy is {0}\n".format(group_exact_accuracy))
         # record.write("-------------------------------------------------------------------------------------------\n")
-    return all_group_top_precision, all_group_top_exact_accuracy, all_group_exact_accuracy
+    return all_group_top_precision, all_group_recall, all_group_top_exact_accuracy, all_group_exact_accuracy
 
 
-def cal_average(all_group_top_precision, all_group_top_exact_accuracy, all_group_accuracy, record):
+
+
+
+def cal_average(all_group_top_precision, all_group_recall, all_group_top_exact_accuracy, all_group_accuracy, record):
     totle = len(all_group_top_precision)
     average_group_top_precision = sum(all_group_top_precision)/totle
+    average_group_recall = sum(all_group_recall)/totle
     average_group_top_exact_accuracy = sum(all_group_top_exact_accuracy)/totle
     average_group_accuracy = sum(all_group_accuracy)/totle
     # record.write("\nthe average group top precision is {0}\n".format(average_group_top_precision))
     record.write("the average group top precision is {0}\n".format(average_group_top_precision))
+    record.write("the average group recall is {0}\n".format(average_group_recall))
     record.write("the average group top exact accuracy is {0}\n".format(average_group_top_exact_accuracy))
     record.write("the average group accuracy is {0}\n".format(average_group_accuracy))
     # record.write("-------------------------------------------------------------------------------------------\n\n")
-
